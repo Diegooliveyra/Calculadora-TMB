66a85ba01d2cd32d83bf96c5fd621354426b508c
Diego Oliveira <diegooliveirabryto@gmail.com>
2020-10-03 11:17:49 -0300

    finalzando script js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,71 @@
-let $weight = document.querySelector('[data-js="weight"]');
-let $age = document.querySelector('[data-js="age"]');
-let $height = document.querySelector('[data-js="height"]');
-let $gender = document.querySelector('[data-js="gender"]');
-let $form = document.querySelector('[data-js="form-calculator"]');
-let $level = document.querySelector('[data-js="level"]');
-let $btnForm = document.querySelector('[data-js="btn-form"]')
+const $weight = document.querySelector('[data-js="weight"]');
+const $age = document.querySelector('[data-js="age"]');
+const $height = document.querySelector('[data-js="height"]');
+const $gender = document.querySelectorAll('input[name="gender"]');
+const $form = document.querySelector('[data-js="form-calculator"]');
+const $level = document.querySelector('[data-js="level"]');
+const $btnForm = document.querySelector('[data-js="btn-form"]');
+const $btnToBack = document.querySelector('[data-js="btn-to-back"]');
+const $metabolismo = document.querySelector('[data-js="metabolismo"]');
+const $keepWeight = document.querySelector('[data-js="keep-weight"]');
+const $loseWeigth = document.querySelector('[data-js="lose-weigth"]');
+const $gainWeight = document.querySelector('[data-js="gain-weight"]');
 
-$form.addEventListener('submit', function(e){
+$form.addEventListener("submit", app, false);
+$btnToBack.addEventListener("submit", backToForm, false);
+
+function app(e) {
   e.preventDefault();
-},false)
+  const genderValue = getRadioValue();
+  $metabolismo.value = calculatorTbm(genderValue);
+  $keepWeight.value = keepWeight();
+  $loseWeigth.value = loseWeigth();
+  $gainWeight.value = gainWeight();
+  document.querySelector("#togle").classList.add("disable");
+  document.querySelector("#togle-result").classList.remove("disable");
+}
+
+function backToForm(e) {
+  e.preventDefault();
+  console.log("ola");
+  document.querySelector("#togle").classList.remove("disable");
+  document.querySelector("#togle-result").classList.add("disable");
+}
+
+function getRadioValue() {
+  for (var i = 0, l = $gender.length; i < l; i++) {
+    if ($gender[i].checked) {
+      return $gender[i].value;
+    }
+  }
+}
+
+function calculatorTbm(genderValue) {
+  if (genderValue === "man") {
+    return Math.round(
+      66 + 13.7 * $weight.value + 5 * $height.value - 6.8 * $age.value
+    );
+  }
+  return Math.round(
+    655 + 9.6 * $weight.value + 1.8 * $height.value - 4.7 * $age.value
+  );
+}
+
+const activityLevel = {
+  sedentario: 1.2,
+  leve: 1.375,
+  moderado: 1.55,
+  pesado: 1.725,
+};
+
+function keepWeight() {
+  return Math.ceil(activityLevel[$level.value] * $metabolismo.value);
+}
+
+function loseWeigth() {
+  return Math.round(Number($keepWeight.value) - 0.15 * $keepWeight.value);
+}
+
+function gainWeight() {
+  return Math.round(Number($keepWeight.value) + 0.15 * $keepWeight.value);
+}
